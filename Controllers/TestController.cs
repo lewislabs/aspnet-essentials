13b71036b57684dd742d283f846459eeedf73a0c
Chris Lewis <chris.lewis@hudl.com>
2018-10-09 13:58:30 +0100

    Fix with attribute routing

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace aspnet_essentials
 {
+    [Route("test")]
     public class TestController : Controller
     {
+        [HttpGet("test")]
         public IActionResult Test()
         {
             return new ContentResult() { Content = "Hello World\n", ContentType = "application/json", StatusCode = 200 };
